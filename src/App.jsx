563907ddf379ab0758c3c5da4976915fc2ce2b67
rudinandrey <rudinandrey@yandex.ru>
2021-10-24 02:41:21 +0300

    add some p

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function(props) {
-    return <h1>Hello World!!!!!</h1>
+    return <div>
+        <h1>Hello World!!!!!</h1>
+        <p>Some text</p>
+    </div>
 }
